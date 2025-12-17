074834266dd3a8dc2e00ecd3ed61ce94a2a8dcc5
PritishRaman <140381361+PritishRaman@users.noreply.github.com>
2025-12-17 10:26:30 +0000

    lecture 2

---
+++ .devcontainer/01Basics/test.js/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId=1442
+let accountEmail="pritishraman@hotmail.com"
+let accountState="Bihar"
+let accountGender="Male"
+
+
+console.table([accountId,accountGender,accountEmail,accountState])
+
+/* prefer not to use var due to block and functional scope
+
+var accountPassword ="1234"
+
+
+accountLocation = "Bihar" //not allowed
+*/
