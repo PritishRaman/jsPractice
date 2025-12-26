13dcff3110c7e1213d89ce456f036e019cb1a124
PritishRaman <140381361+PritishRaman@users.noreply.github.com>
2025-12-26 09:50:00 +0000

    revised

--- .devcontainer/01Basics/test.js/01_Variables.js
+++ .devcontainer/01Basics/test.js/01_Variables.js
@@ -1,12 +1,18 @@
-const accountId=1442
+//console.log("hello") //print statement 
+
+
+const accountId=1442 // Used to declare values that cant be changed later
 let accountEmail="pritishraman@hotmail.com"
 let accountState="Bihar"
 let accountGender="Male"
+hello = 123
+
+console.log(hello)
 
 
-console.table([accountId,accountGender,accountEmail,accountState])
+console.table([accountId,accountGender,accountEmail,accountState]) // prints in the form of a table
 
-/* prefer not to use var due to block and functional scope
+/* prefer not to use 'var' due to block and functional scope
 
 var accountPassword ="1234"
 
